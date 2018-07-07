2720522c9781a781b94b6b3960fbcd1c72ce1d83
Chris McKenzie <kristopolous@yahoo.com>
2018-07-07 03:35:46 -0700

    cleaning up bento

--- run/bootstrap/index.js
+++ run/bootstrap/index.js
@@ -3,18 +3,13 @@
 let cluster = require('cluster');
 let log     = Bento.Log;
 
-/**
- * @method _bootstrap
- */
 Bento._bootstrap = function *() {
 
   // ### Share Loader
   // Load API components shared on both master, and worker processes.
 
-  yield require('./models');
-  yield require('./providers');
-  yield require('./interface');
-  yield require('./hooks');
+  ['./models','./providers','./interface','./hooks']
+    .forEach((row) => (yield require(row))(); );
 
   // ### Worker Loader
   // Load API components for worker processes only.
@@ -25,12 +20,4 @@ Bento._bootstrap = function *() {
     yield require('./errors');
     yield require('./routes');
   }
-
-  // ### Test Loader
-  // Load all registered tests under the modules, and providers.
-
-  if (Bento.isTesting()) {
-    yield require('./tests');
-  }
-
 };
